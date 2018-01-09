026b7074de4dabf1a8a6cdc30b5f67aec55262cf
princeDentro <b.gonzaga.caranoo@gmail.com>
2018-01-08 17:26:31 -0800

    change database

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,27 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+/*$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'benjie101',
+	'password' => 'A0xQ4weh{gdS',
+	'database' => 'benjie_testing',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'dbport' => '3306',
+	'failover' => array(),
+	'save_queries' => TRUE
+);*/
